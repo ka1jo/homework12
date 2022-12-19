8d33234837cd60d370323ff7897d113a350970ae
Пользователь <pitann3@gmail.com>
2022-12-19 23:39:14 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var buying =[
+
+    ["samsung", "iphone" ,"xiaomi"],
+    ["RocketLeague","warcraft","dota2","fallout"],
+    ["sweatshirt","hat","shoes","duffel"]
+] 
+
+console.log("my name is"+" "+buying[1][0][0]+buying[0][0][1]+buying[1][3][0]+buying[0][2][2]+buying[1][0][4]+buying[2][3][5])
